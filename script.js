f598dfd0f0fd48e7f7c24199cf79405b08e6db69
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-08-01 20:06:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+
+function removeColor() {
+	let clickedElement=document.getElementById('colorSelect');
+	let index=clickedElement.selectedIndex;
+	if(index>-1){
+		clickedElement.remove(index)
+	}
+}
